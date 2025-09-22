3a93fcaacf0311476982edbc906c837b010f5ff3
Jinx <Jinx@DESKTOP-JA5K7KD>
2025-09-22 20:35:10 +0300

    Implemention of DAL

---
+++ C44-G04-MVC03.DAL/Data/Configurations/DepartmentConfg.cs
@@ -0,0 +1,19 @@
+﻿using C44_G04_MVC03.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C44_G04_MVC03.DAL.Data.Configurations
+{
+    internal class DepartmentConfg : IEntityTypeConfiguration<Department>
+    {
+        void IEntityTypeConfiguration<Department>.Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.Property(D => D.Id).UseIdentityColumn(10, 10);
+        }
+    }
+}

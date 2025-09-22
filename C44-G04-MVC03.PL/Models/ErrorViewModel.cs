855585550d5a777be84ddde4fb75ba34dc216846
Jinx <Jinx@DESKTOP-JA5K7KD>
2025-09-22 20:08:16 +0300

    Add project files.

---
+++ C44-G04-MVC03.PL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace C44_G04_MVC03.PL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

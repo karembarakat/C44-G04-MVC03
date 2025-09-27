17bec4aa8756ab1eeddb52d2ae3b9fdc35b45b57
Jinx <Jinx@DESKTOP-JA5K7KD>
2025-09-27 08:21:24 +0300

    Added Employee Views

---
+++ C44-G04-MVC03.PL/Controllers/EmployeeController.cs
@@ -0,0 +1,126 @@
+﻿using C44_G04_MVC03.BLL.Interfaces;
+using C44_G04_MVC03.DAL.Models;
+using C44_G04_MVC03.DAL.Modles;
+using C44_G04_MVC03.PL.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace C44_G04_MVC03.PL.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var employees = _employeeRepository.GetAll();
+            return View(employees);
+        }
+
+        [HttpGet]
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Create(CreateEmployeeDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var employee = new Employee()
+                {
+
+                    Name = model.Name,
+                    Email = model.Email,
+                    Address = model.Address,
+                    Phone = model.Phone,
+                    Salary = model.Salary,
+                    IsActive = model.IsActive,
+                    isDeleted = model.isDeleted,
+                    Age = model.Age,
+                    HireDate = model.HireDate,
+                    CreateAt = model.CreateAt
+                };
+
+                var count = _employeeRepository.Add(employee);
+                if (count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+
+
+            }
+
+            return View(model);
+        }
+
+
+        [HttpGet]
+        public IActionResult Details(int id, string viewName = "Details")
+        {
+            var employee = _employeeRepository.Get(id);
+            if (employee == null)
+                return NotFound();
+            return View(viewName, employee);
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+
+            return Details(id, "Edit");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit([FromRoute] int id, Employee model)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                if (id != model.Id) return BadRequest(); // 400
+                var count = _employeeRepository.Update(model);
+                if (count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+
+            return View(model);
+        }
+
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            return Details(id, "Delete");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete([FromRoute] int id, Employee model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (id != model.Id) return BadRequest(); // 400
+                var count = _employeeRepository.Delete(model);
+
+                if (count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+
+            }
+
+            return View(model);
+        }
+
+    }
+}

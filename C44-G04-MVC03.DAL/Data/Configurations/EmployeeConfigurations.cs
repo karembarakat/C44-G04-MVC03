8fdb1364f196d1936b3eac10f8b51f71328aff54
Jinx <Jinx@DESKTOP-JA5K7KD>
2025-09-27 04:42:28 +0300

    Implementaion Of Dal for Employee

---
+++ C44-G04-MVC03.DAL/Data/Configurations/EmployeeConfigurations.cs
@@ -0,0 +1,19 @@
+﻿using C44_G04_MVC03.DAL.Modles;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C44_G04_MVC03.DAL.Data.Configurations
+{
+    public class EmployeeConfigurations : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder.Property(E => E.Salary).HasColumnType("decimal(18,2)");
+        }
+    }
+}
